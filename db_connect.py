c908f88aeaca13a69468585151cfa80a6324d18d
Abhijit Krishna Menon <akm5630@gmail.com>
2023-01-15 17:02:01 -0800

    Script Updates

--- db_connect.py
+++ db_connect.py
@@ -1,9 +1,20 @@
 import sqlite3
-con = sqlite3.connect('mlops_db.db')
-cur = con.cursor()
+import pandas 
+
+def connect_to_database(db_name):  #'mlops_db.db'
+    connection_db = sqlite3.connect(db_name)
+    cur = con.cursor()
+    return cur 
+
+def run_query(query,db_name):
+    connection = connect_to_database(db_name)
+    for row in connection.execute(query):
+        print(row)
 
 
+if __name__ == '__main__':
+    db_name = 'mlops_db.db'
+    query = 'SELECT * FROM MLOPS_TEST_TABLE ORDER BY id;'
+    run_query(query,db_name)
 
-for row in cur.execute('SELECT * FROM MLOPS_TEST_TABLE ORDER BY id;'):
-    print(row)
 
